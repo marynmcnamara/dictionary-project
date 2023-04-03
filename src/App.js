0c0aafd0c8b80c3b3f4af66c42fe6a1f20a7c045
marynmcnamara <119798224+marynmcnamara@users.noreply.github.com>
2023-04-03 12:49:29 -0400

    created dictionary app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Dictionary from "./Dictionary";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <Dictionary defaultKeyword="tropical" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/marynburns"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Maryn McNamara
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/marynmcnamara/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://delightful-gumdrop-faeb0a.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
